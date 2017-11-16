53791df3fbc430aca4d93e8ff2d3dbc874134042
Dustin Byrne <dbyrne@conjur.net>
2017-11-16 17:48:33 -0500

    return 409 if app is already bound

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+  rescue_from ServiceBinding::RoleAlreadyCreated, with: :conflict_error
+
+  def conflict_error e
+    logger.warn(e)
+    head :conflict
+  end
 end
--- app/models/service_binding.rb
+++ app/models/service_binding.rb
@@ -1,10 +1,21 @@
 require 'conjur_client'
 
 class ServiceBinding
+  class RoleAlreadyCreated < RuntimeError
+  end
+
   class << self
     def create instance_id, binding_id, app_id
+      host = conjur_api.role role_name(binding_id, app_id)
+      raise RoleAlreadyCreated if host.exists?
+
       conjur_api.load_policy 'root', add_policy_template(binding_id)
-      conjur_api.load_policy binding_id, bind_template(app_id)
+      res = conjur_api.load_policy binding_id, bind_template(app_id)
+
+      return {
+        :authn_login => "host/#{binding_id}/#{app_id}",
+        :authn_api_key => res.created_roles.values.first['api_key']
+      }
     end
 
     def delete instance_id, binding_id, app_id
@@ -32,6 +43,10 @@ class ServiceBinding
       - !host
         id: #{app_id}
       """
+    end
+
+    def role_name binding_id, app_id
+      "#{ConjurClient.account}:host:#{binding_id}/#{app_id}"
     end
 
     def conjur_api
