e66608ea77e62daee766ff8d059902cbd54908aa
Dustin Byrne <dbyrne@conjur.net>
2017-11-16 18:19:45 -0500

    remove extra policy record, initialize host only

--- app/models/service_binding.rb
+++ app/models/service_binding.rb
@@ -9,44 +9,35 @@ class ServiceBinding
       host = conjur_api.role role_name(binding_id, app_id)
       raise RoleAlreadyCreated if host.exists?
 
-      conjur_api.load_policy 'root', add_policy_template(binding_id)
-      res = conjur_api.load_policy binding_id, bind_template(app_id)
+      res = conjur_api.load_policy 'root', template_create(binding_id)
 
       return {
-        :authn_login => "host/#{binding_id}/#{app_id}",
+        :authn_login => "host/#{binding_id}",
         :authn_api_key => res.created_roles.values.first['api_key']
       }
     end
 
     def delete instance_id, binding_id, app_id
       conjur_api.load_policy 'root', 
-                             delete_policy_template(binding_id),
+                             template_delete(binding_id),
                              method: Conjur::API::POLICY_METHOD_PATCH
     end
 
-    def add_policy_template binding_id
-      """
-      - !policy
-        id: #{binding_id}
-      """
-    end
-
-    def delete_policy_template binding_id
+    def template_delete binding_id
       """
       - !delete
-        record: !policy #{binding_id}
+        record: !host #{binding_id}
       """
     end
 
-    def bind_template app_id
+    def template_create binding_id
       """
-      - !host
-        id: #{app_id}
+      - !host #{binding_id}
       """
     end
 
     def role_name binding_id, app_id
-      "#{ConjurClient.account}:host:#{binding_id}/#{app_id}"
+      "#{ConjurClient.account}:host:#{binding_id}"
     end
 
     def conjur_api
