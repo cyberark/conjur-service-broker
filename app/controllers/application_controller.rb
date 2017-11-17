352a79f4b15a99e29de29fd9f3eb8a151e10ae8e
Dustin Byrne <dbyrne@conjur.net>
2017-11-17 12:06:50 -0500

    broker will respond with a 500 if it's unauthorized to make requests to Conjur

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::API
   rescue_from ServiceBinding::RoleAlreadyCreated, with: :conflict_error
+  rescue_from RestClient::Unauthorized, with: :server_error
 
   def conflict_error e
     logger.warn(e)
     head :conflict
+  end
+
+  def server_error e
+    logger.warn(e)
+    head :internal_server_error
   end
 end
