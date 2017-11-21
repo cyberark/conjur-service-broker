8426ffd5679a19a1c97efdaa003176a3ca7aa32d
John Tuttle <jtuttle@conjur.net>
2017-11-21 17:05:41 -0500

    don't bundle install in test.sh

--- test.sh
+++ test.sh
@@ -2,10 +2,16 @@
 
 # Stand up containers
 # Make sure we keep up to date with gem changes
-docker-compose rm -fs
-docker-compose up -d
-docker-compose exec service-broker bundle install
-docker-compose exec -d service-broker bash -c "export CONJUR_AUTHN_API_KEY=`cat tmp/api_key` && ./bin/rails s -b 0.0.0.0"
+docker-compose rm -f
+
+docker-compose up -d conjur pg
+
+sleep 10
+
+docker-compose up -d service-broker
+
+#docker-compose exec service-broker bundle install
+#docker-compose exec -d service-broker bash -c "export CONJUR_AUTHN_API_KEY=`cat tmp/api_key` && ./bin/rails s -b 0.0.0.0"
 
 finish() {
   docker-compose down
