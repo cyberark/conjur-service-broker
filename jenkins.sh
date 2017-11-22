3d74d8e872ce9739470dea7435523cf521d23c44
John Tuttle <jtuttle@conjur.net>
2017-11-22 11:11:05 -0500

    set api key after rails starts

--- jenkins.sh
+++ jenkins.sh
@@ -3,4 +3,4 @@
 docker-compose rm -fs
 docker-compose up -d
 docker-compose exec service-broker bundle install
-docker-compose exec service-broker bash -c "export CONJUR_AUTHN_API_KEY=`cat tmp/api_key` && ./bin/rails s -b 0.0.0.0"
+docker-compose exec service-broker bash -c "export CONJUR_AUTHN_API_KEY=`cat tmp/api_key`"
