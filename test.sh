e87b8be65413dd88479aef8736ccce57821436f1
John Tuttle <jtuttle@conjur.net>
2017-11-22 13:26:24 -0500

    add version file, move trap to top of test script

--- test.sh
+++ test.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -ex
 
+finish() {
+  docker-compose down
+}
+trap finish EXIT
+
 # Stand up containers
 # Make sure we keep up to date with gem changes
 docker-compose rm -f
@@ -12,10 +17,5 @@ docker-compose up -d service-broker
 
 #docker-compose exec service-broker bundle install
 #docker-compose exec -d service-broker bash -c "export CONJUR_AUTHN_API_KEY=`cat tmp/api_key` && ./bin/rails s -b 0.0.0.0"
-
-finish() {
-  docker-compose down
-}
-trap finish EXIT
 
 docker-compose exec -T service-broker ci/test.sh
