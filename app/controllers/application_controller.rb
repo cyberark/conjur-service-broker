980317e39feb2c76809adc41943456fef5884919
Kumbirai Tanekha <kumbirai.tanekha@gmail.com>
2017-12-08 21:08:44 +0000

    auth woes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::API
   rescue_from ServiceBinding::RoleAlreadyCreated, with: :conflict_error
   rescue_from RestClient::Unauthorized, with: :server_error
 
-  http_basic_authenticate_with name: ENV['SECURITY_USER_NAME'], password: ENV['SECURITY_USER_PASSWORD'] unless ENV['SECURITY_USER_NAME'].blank? && ENV['SECURITY_USER_PASSWORD'].blank?
+  http_basic_authenticate_with name: ENV['AUTH_USERNAME'], password: ENV['AUTH_PASSWORD'] unless ENV['AUTH_USERNAME'].blank? && ENV['AUTH_PASSWORD'].blank?
 
   def conflict_error e
     logger.warn(e)
@@ -14,14 +14,5 @@ class ApplicationController < ActionController::API
   def server_error e
     logger.warn(e)
     head :internal_server_error
-  end
-
-  private
-
-  def authenticate
-    username, password = ENV['SECURITY_USER_NAME'], ENV['SECURITY_USER_PASSWORD']
-    if username.present? || password.present?
-      http_basic_authenticate_with(name: username, password: password)
-    end
   end
 end
