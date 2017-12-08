24f044a2f7e59e08a28c6c28628c4c004a51e935
Kumbirai Tanekha <kumbirai.tanekha@gmail.com>
2017-12-08 20:56:18 +0000

    back to basics :p

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -1,18 +1,5 @@
 class CatalogController < ApplicationController
   def get
-    render json: catalog
-  end
-
-  private
-
-  def catalog
-    @CATALOG ||= generate_catalog
-  end
-
-  def generate_catalog
-    catalog = Rails.application.config.service_broker['catalog']
-    catalog['services'][0]['name'] = ENV['SERVICE_NAME'] || catalog['services'][0]['name']
-    catalog['services'][0]['id'] = ENV['SERVICE_ID'] || catalog['services'][0]['id']
-    catalog
+    render json: Rails.application.config.service_broker['catalog']
   end
 end
