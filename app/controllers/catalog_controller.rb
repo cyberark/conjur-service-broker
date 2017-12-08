e257af6cd0fd93278cc702db2f84301a3cd0b4c4
Kumbirai Tanekha <kumbirai.tanekha@gmail.com>
2017-12-08 17:46:07 +0000

    Service Broker returns its catalog CONJ-4482

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -1,5 +1,19 @@
 class CatalogController < ApplicationController
   def get
-    render json: Rails.application.config.service_broker['catalog']
+    render json: catalog
+  end
+
+  private
+
+  def catalog
+    @CATALOG ||= generate_catalog
+    @CATALOG
+  end
+
+  def generate_catalog
+    catalog = Rails.application.config.service_broker['catalog']
+    catalog['services'][0]['name'] = ENV['SERVICE_NAME'] || catalog['services'][0]['name']
+    catalog['services'][0]['id'] = ENV['SERVICE_ID'] || catalog['services'][0]['id']
+    catalog
   end
 end
