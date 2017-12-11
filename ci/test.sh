9f910f47096401aed63fa953a0773067b213bf50
Jason Vanderhoof <jvanderhoof@gmail.com>
2017-12-11 16:07:35 -0700

    Added todo to track report export

--- ci/test.sh
+++ ci/test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -ex
 
-rspec # --format RspecJunitFormatter --out spec/reports/test.xml --format progress
+# TODO - The commented arguments are throwing an exeception unreleated to the
+# success of the RSpec test.  We need to get this resolved and export the
+# resulting XML file to the `spec/reports` folder for Jenkins to pickup.
+rspec # --format RspecJunitFormatter --out spec/reports/test.xml
 
 cucumber --format junit --out features/reports  --format pretty --backtrace --verbose
