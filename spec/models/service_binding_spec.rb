dba0d2541a94025794a389c283f648713b6a7479
John Tuttle <jtuttle@conjur.net>
2018-01-05 17:08:23 -0500

    remove spec test for private method

--- spec/models/service_binding_spec.rb
+++ spec/models/service_binding_spec.rb
@@ -1,10 +1,5 @@
 require 'spec_helper'
 
 describe ServiceBinding do
-  describe "template_delete" do
-    it "returns policy lines to delete the host" do
-      expect(ServiceBinding.template_delete('binding-id')).to include("delete")
-      expect(ServiceBinding.template_delete('binding-id')).to include("!host binding-id")
-    end
-  end
+
 end
