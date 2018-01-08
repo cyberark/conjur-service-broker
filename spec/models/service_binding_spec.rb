c7a67e2a8705e345a9bc5626ce9e055e56ab406c
John Tuttle <jtuttle@conjur.net>
2018-01-08 14:47:40 -0500

    Service broker bind returns correct status code (#13)
    * stub out tests, add 201 response status
    
    * create step def for binding
    
    * add 409 conflict test
    
    * DRY up error handling
    
    * rename call api method
    
    * clean up service binding model
    
    * add 422 error case
    
    * log missing app guid, error message for host existing
    
    * remove spec test for private method
    
    * replace spec, rename method, formatting

--- spec/models/service_binding_spec.rb
+++ spec/models/service_binding_spec.rb
@@ -1,10 +1,26 @@
 require 'spec_helper'
 
 describe ServiceBinding do
-  describe "template_delete" do
-    it "returns policy lines to delete the host" do
-      expect(ServiceBinding.template_delete('binding-id')).to include("delete")
-      expect(ServiceBinding.template_delete('binding-id')).to include("!host binding-id")
+  describe "delete" do
+    let(:host) { double("host") }
+    
+    before do
+      allow(host).to receive(:exists?).and_return(true)
+      allow_any_instance_of(Conjur::API).to receive(:resource).and_return(host)
+    end
+    
+    it "uses the Conjur API to load policy to delete the host" do
+      expect_any_instance_of(Conjur::API).
+        to receive(:load_policy).
+        with("root",
+    """
+    - !delete
+      record: !host binding_id
+    """,
+        method: :patch
+        )
+      
+      ServiceBinding.new("service_id", "binding_id").delete
     end
   end
 end
