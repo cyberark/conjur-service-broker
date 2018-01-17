f28e7cdb5626567117627ff79423b323b9255c14
Geri Jennings <geri.jennings@cyberark.com>
2018-01-17 11:16:59 -0500

    updates to provision spec

--- spec/controllers/provision_controller_spec.rb
+++ spec/controllers/provision_controller_spec.rb
@@ -3,25 +3,34 @@ require 'spec_helper'
 describe ProvisionController, :type => :controller do
   describe '#put' do
     def provision_put(instance_id, space_guid)
-      allow_any_instance_of(ProvisionController).to receive(:instance_id) { instance_id }
       allow_any_instance_of(ProvisionController).to receive(:space_guid) { space_guid }
+      allow_any_instance_of(ProvisionController).to receive(:authenticate).and_return(true)
 
-      put :put
+      headers = { 'Content-Type' => 'application/json',
+        'X-Broker-API-Version' => '2.13' }
+      request.headers.merge! headers
+      put :put, params: { instance_id: instance_id }
     end
 
     context 'standard provision' do
       it "sets a single instance/space map" do
         provision_put('sample_instance_id', 'sample_space_guid')
         expect(response).to be_ok
+
         expect(@@instance_id_to_space_guid).to have_key['sample_instance_id']
         expect(@@instance_id_to_space_guid['sample_instance_id']).to eq 'sample_space_guid'
       end
 
       it "sets multiple instance/space maps" do
-        put :put, instance_id: 'original_instance_id', space_guid: 'original_space_guid'
+        provision_put('original_instance_id', 'original_space_guid')
         expect(response).to be_ok
-        put :put, instance_id: 'new_instance_id', space_guid: 'new_space_guid'
+
+        expect(@@instance_id_to_space_guid).to have_key['original_instance_id']
+        expect(@@instance_id_to_space_guid['original_instance_id']).to eq 'original_space_guid'
+
+        provision_put('new_instance_id', 'new_space_guid')
         expect(response).to be_ok
+
         expect(@@instance_id_to_space_guid).to have_key['original_instance_id']
         expect(@@instance_id_to_space_guid['original_instance_id']).to eq 'original_space_guid'
         expect(@@instance_id_to_space_guid).to have_key['new_instance_id']
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 
-Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+Dir[Rails.root.join("spec/*.rb")].each { |f| require f }
 
 RSpec.configure do |config|
   config.infer_base_class_for_anonymous_controllers = false
