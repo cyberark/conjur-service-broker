2c3e515a19dd8bf5271d67b642d5c4ff792c0f50
Geri Jennings <geri.jennings@cyberark.com>
2018-01-29 14:58:07 -0500

    first pass and restructuring tests for v4 / v5

--- test.sh
+++ test.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -ex
 
-docker-compose down --rmi 'local' --volumes
-
 function finish {
   echo 'Removing test environment'
   echo '---'
@@ -10,15 +8,36 @@ function finish {
 }
 trap finish EXIT
 
-docker-compose up -d conjur pg
+function main() {
 
-docker-compose exec -T conjur conjurctl wait -r 30 -p 80
-api_key=$(docker-compose exec -T conjur bash -c 'rails r "puts Role[%Q{cucumber:user:admin}].api_key" 2>/dev/null')
-export CONJUR_AUTHN_API_KEY="$api_key"
+  startConjur
+  runTests5
+#  runTests4
+}
 
-# load the pcf policy for the non-empty CONJUR_POLICY test
-docker-compose run --rm --entrypoint bash client -c "conjur policy load root /app/ci/policy.yml"
-export CONJUR_POLICY=pcf
+function startConjur() {
+  echo "Starting Conjur environment"
+  echo "---"
+  docker-compose up -d pg conjur_4 conjur_5
+}
 
-docker-compose up -d conjur-service-broker service-broker-bad-url service-broker-bad-key service-broker-alt-policy
-docker-compose run tests ci/test.sh
+function runTests5() {
+  echo "Waiting for Conjur v5 to come up, and configuring it..."
+  ./ci/configure_v5.sh
+
+  api_key=$(docker-compose exec -T conjur_5 bash -c 'rails r "puts Role[%Q{cucumber:user:admin}].api_key" 2>/dev/null')
+  export CONJUR_AUTHN_API_KEY="$api_key"
+
+  # load the pcf policy for the non-empty CONJUR_POLICY test
+  docker-compose run --rm --entrypoint bash client -c "conjur policy load root /app/ci/policy.yml"
+  export CONJUR_POLICY=pcf
+
+  docker-compose up -d conjur-service-broker service-broker-bad-url service-broker-bad-key service-broker-alt-policy
+  docker-compose run tests ci/test.sh
+}
+
+function runTests4() {
+  echo "running the v4 tests"
+}
+
+main
