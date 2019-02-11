1068828512b021d60a696b8cee2afdfc33520664
Micah Lee <micah.lee@cyberark.com>
2019-02-11 16:16:41 -0500

    Configure separate follower URL for V4 vs V5

--- features/step_definitions/json_steps.rb
+++ features/step_definitions/json_steps.rb
@@ -1,3 +1,7 @@
 Then(/^the JSON at "([^"]*)" should be the master address$/) do |path|
   step "the JSON at \"#{path}\" should be \"#{ENV['CONJUR_APPLIANCE_URL']}\""
 end
+
+Then(/^the JSON at "([^"]*)" should be the follower address$/) do |path|
+  step "the JSON at \"#{path}\" should be \"#{ENV['CONJUR_FOLLOWER_URL']}\""
+end
