ddcf1b9e066518441ffdb4b35451c4f74d69a719
Micah Lee <micah.lee@cyberark.com>
2019-04-12 16:48:04 -0400

    only delete a CI org if the scenario passes for easier debugging

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -6,6 +6,9 @@ Before("@conjur-version-4") do
   skip_this_scenario unless ENV['CONJUR_VERSION'] == "4"
 end
 
-After("@integration") do
-  cf_delete_org(cf_ci_org)
+After("@integration") do |scenario|
+  if scenario.status == :passed
+    cleanup_service_broker
+    cf_delete_org(cf_ci_org)
+  end
 end
