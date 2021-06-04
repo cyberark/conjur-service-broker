3ed009a8650fb3032f0e5b605dfdaef4740d69cb
Miclain K Keffeler <mkkeffeler@crimson.ua.edu>
2021-06-04 10:02:18 -0500

    Adding Org & Space Name Annotations (#248)
    * adding org and space name annotations
    
    Signed-off-by: Miclain Keffeler <mickeffeler@gmail.com>
    
    * parse changelog fix.
    
    Signed-off-by: Miclain Keffeler <mickeffeler@gmail.com>
    
    * fix invalid JSON.
    
    Signed-off-by: Miclain Keffeler <mickeffeler@gmail.com>
    
    Co-authored-by: Miclain Keffeler <mickeffeler@gmail.com>

--- features/provision.feature
+++ features/provision.feature
@@ -45,6 +45,29 @@ Feature: Provisioning
       }
       """
 
+
+  @conjur-version-5
+  Scenario: Provision resource with service broker API 2.15 or better
+    When I PUT "/v2/service_instances/9b292a9c-af66-4797-8d98-b30801f32ax7" with body:
+    """
+    {
+      "context": {
+        "organization_guid": "e027f3f6-80fe-4d22-9374-da23a035ba0a",
+        "space_guid": "8c56f85c-c16e-4158-be79-5dac74f970db",
+        "organization_name": "my-organization",
+        "space_name": "my-space"
+      },
+      "service_id": "c024e536-6dc4-45c6-8a53-127e7f8275ab",
+      "plan_id": "3a116ac2-fc8b-496f-a715-e9a1b205d05c.community",
+      "organization_guid": "e027f3f6-80fe-4d22-9374-da23a035ba0a",
+      "space_guid": "8c56f85c-c16e-4158-be79-5dac74f970db",
+      "parameters": {
+      }
+    }
+    """
+    Then the HTTP response status code is "201"
+    And the JSON should be {}
+
   @conjur-version-5
   Scenario: Provision resource
     When I PUT "/v2/service_instances/9b292a9c-af66-4797-8d98-b30801f32ax7" with body:
---
+++ spec/models/org_space_policy_spec_no_annotations.rb
@@ -0,0 +1,106 @@
+require 'spec_helper'
+
+describe OrgSpacePolicy do
+
+  let(:org_guid) { "my_org" }
+  let(:space_guid) { "my_space" }
+  let(:policy_name) { 'cf' }
+
+  subject { OrgSpacePolicy.new(org_guid, space_guid, nil, nil) }   
+
+  before do
+    allow(ConjurClient).to receive(:policy).and_return(policy_name)    
+    allow(ConjurClient).to receive(:login_host_id).and_return('service-broker')  
+
+    allow(ENV).to receive(:[]).and_call_original
+  end
+
+  let(:create_policy_body) do
+    <<~YAML
+      ---
+      - !policy
+        id: #{org_guid}
+        body:
+          - !layer
+
+          - !policy
+            id: #{space_guid}
+            body:
+              - !layer
+
+          - !grant
+            role: !layer
+            member: !layer #{space_guid}
+      YAML
+  end
+
+  describe "#create" do
+    context "when org and space names are not available from Service Broker API" do
+      it "loads Conjur policy to create the org and space policy without annotations" do
+        expect_any_instance_of(Conjur::API).
+          to receive(:load_policy).
+          with(policy_name, create_policy_body, method: :post)
+
+        subject.create
+      end
+    end
+  end
+
+  describe "#ensure_exists" do
+    let(:existent_resource) { double("existent", exists?: true) }
+    let(:nonexistent_resource) { double("non-existent", exists?: false) }
+  
+    before do
+      # By default, assume all resources exist
+      allow_any_instance_of(Conjur::API)
+        .to receive(:resource)
+        .with(any_args)
+        .and_return(existent_resource)
+    end
+
+    context "when resources already exist" do
+      it "does not raise an error" do
+        expect { subject.ensure_exists }.not_to raise_error
+      end
+    end
+
+    context "when org policy doesn't exist" do
+      before do
+        allow_any_instance_of(Conjur::API)
+          .to receive(:resource)
+          .with("cucumber:policy:#{policy_name}/#{org_guid}").
+          and_return(nonexistent_resource)
+      end
+
+      it "raises an error" do
+        expect { subject.ensure_exists }.to raise_error(OrgSpacePolicy::OrgPolicyNotFound)
+      end
+    end
+
+    context "when space policy doesn't exist" do
+      before do
+        allow_any_instance_of(Conjur::API)
+          .to receive(:resource)
+          .with("cucumber:policy:#{policy_name}/#{org_guid}/#{space_guid}")
+          .and_return(nonexistent_resource)
+      end
+
+      it "raises an error" do
+        expect { subject.ensure_exists }.to raise_error(OrgSpacePolicy::SpacePolicyNotFound)
+      end
+    end
+
+    context "when space layer doesn't exist" do
+      before do
+        allow_any_instance_of(Conjur::API)
+          .to receive(:resource)
+          .with("cucumber:layer:#{policy_name}/#{org_guid}/#{space_guid}")
+          .and_return(nonexistent_resource)
+      end
+
+      it "raises and error" do
+        expect { subject.ensure_exists }.to raise_error(OrgSpacePolicy::SpaceLayerNotFound)
+      end
+    end
+  end
+end
