bba186fffb68eba078e093dc77f621a6b6d61bcc
Shlomo Heigh <shlomo.heigh@cyberark.com>
2024-07-26 10:02:58 -0400

    Document running e2e tests manually

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,7 +6,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
-## [1.2.11] - 2024-07-23
+## [1.2.11] - 2024-07-26
 
 ### Security
 - Upgrade Ruby to 3.3, Go to 1.22, and various other dependencies
--- config/puma.rb
+++ config/puma.rb
@@ -8,6 +8,7 @@ threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 8080.
+# Note: Use the default port so the TAS health check can find the app.
 #
 port        ENV.fetch("PORT") { 8080 }
 
